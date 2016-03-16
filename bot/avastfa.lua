5cbb51ec24790b2d6311a0410f5ea4e540a6c577
telespead <bot.tele.spead@gmail.com>
2016-03-16 07:52:17 +0330

    Update avastfa.lua

--- bot/avastfa.lua
+++ bot/avastfa.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {96700621},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
